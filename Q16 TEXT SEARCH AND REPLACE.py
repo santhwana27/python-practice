9984950628b3bd407db7adcc084f9358112dbeac
Santhwana P <santhwanap279@gmail.com>
2026-02-12 15:09:11 -0300

    Add files via upload

---
+++ Q16 TEXT SEARCH AND REPLACE.py
@@ -0,0 +1,11 @@
+f = open("exam.txt", "r")
+text = f.read()
+f.close()
+
+old_word = input("Enter the old text: ")
+new_word = input("Enter the new text: ")
+updated_text = text.replace(old_word, new_word)
+f = open("exam.txt", "w")
+f.write(updated_text)
+f.close()
+print("All occurrences of", old_word, "replaced with", new_word)
